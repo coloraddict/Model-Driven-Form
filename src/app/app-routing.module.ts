c93f99e5ce1488677ad71e27e62d205159a9655e
Ajay Jaiswar <ajayvjaiswar@gmail.com>
2020-11-28 17:54:25 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ModelFormComponent } from './model-form/model-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'modelform', component: ModelFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
